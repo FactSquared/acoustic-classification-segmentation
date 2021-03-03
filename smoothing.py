127f55c25f04a854d7d84b05dfc6fab75d6df22b
Liz Fischer <lizzy.m.fischer@gmail.com>
2021-03-03 14:34:50 -0600

    Fix merge function
    Hopefully...

--- smoothing.py
+++ smoothing.py
@@ -9,25 +9,39 @@ def smooth(predictions, threshold=0, binary=False):
     return grouped
 
 
-def merge_short_sounds(predictions, threshold):  # TODO: Doesn't catch short start or end (see jazz eg)
-    if len(predictions) * FRAME_SIZE < threshold:  # if audio is shorter than the threshold, do nothing
-        return predictions
+def merge_short_sounds(predictions, threshold): #TODO: test.. I'm like 95% sure this works
+    merged = []
+    min_n_frames = round(threshold/FRAME_SIZE)
 
-    merged = predictions
-    i = 0
-    while i < len(predictions):
-        current = merged[i]
-        next_different = np.where(predictions[i:] != current)[0]
-        if len(next_different) == 0:  # if no more changes
-            seg_length = len(predictions)-1 - i
-            i += 1  # prevent infinite loop
+    previous_value = None
+    current_seg_start = 0
+
+    while current_seg_start < len(predictions):
+        current_value = predictions[current_seg_start]
+        find_next = np.where(predictions[current_seg_start:] != current_value)[0]
+        if find_next.size == 0: # if last one
+            current_seg_len = len(predictions) - current_seg_start
+            next_different = len(predictions)
         else:
-            seg_length = next_different[0]
-        if seg_length * FRAME_SIZE < threshold:
-            merged[i:i+seg_length] = merged[i-1]  # replace short seg with previous value
-        i += seg_length
-    return merged
+            current_seg_len = find_next[0]
+            next_different = current_seg_len + current_seg_start
 
+        if current_seg_len >= min_n_frames: # if it's big enough, add & move on
+            segment = predictions[current_seg_start:next_different]
+            previous_value = current_value
+            #print(f"Added {current_seg_len} frames of {current_value}")
+        else: # too small
+            if not previous_value: # if there hasn't been a long segment yet, just move on
+                current_seg_start = next_different
+               # print(f"Initial segment, {current_seg_len} frames of {current_value}, skipped")
+                continue
+            segment = [previous_value]*current_seg_len # make segment the same size w/ previous value
+            #print(f"Changed {current_seg_len} frames of {current_value} to {previous_value}")
+
+        merged.extend(segment)
+        current_seg_start = next_different
+
+    return merged
 
 def group_frames(predictions, binary):
     i = 0
